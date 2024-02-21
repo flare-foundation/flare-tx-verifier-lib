1d40498f7efe17a1bc3ab40c758755122e9b9a14
Jan <jan@LAPTOP-JAN>
2024-02-21 19:47:59 +0100

    refactoring

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -2,12 +2,12 @@ import { verify } from "../src"
 
 async function main() {
     let txHex = process.argv[2].trim().toLowerCase()
-        let verification = await verify(txHex)
-        if (verification == null) {
-            console.log("Transaction verification did not succeed")
-        } else {
-            console.log(JSON.stringify(verification, null, "  "))
-        }
+    let verification = await verify(txHex)
+    if (verification == null) {
+        console.log("Transaction verification did not succeed")
+    } else {
+        console.log(JSON.stringify(verification, null, "  "))
+    }
 }
 
 main()
